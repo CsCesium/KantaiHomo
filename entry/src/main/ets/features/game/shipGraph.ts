0ee802b3373e27e74904af307aeaae202d80e7d3
YU_ZEJING <cs_cesium@outlook.com>
2026-04-21 20:39:32 +0800

    update: 1. correct the obfuscation algorithm(NEED TEST)

--- entry/src/main/ets/features/game/shipGraph.ts
+++ entry/src/main/ets/features/game/shipGraph.ts
@@ -5,11 +5,13 @@
 
 //Tanaka's magic
 const RESOURCE = [
-  6, 5, 3, 1, 4, 2, 3, 8, 4, 2, 3, 2, 6, 1, 7, 7, 2, 9, 2, 6,
-  4, 8, 4, 5, 6, 9, 8, 3, 4, 9, 7, 4, 6, 3, 1, 5, 6, 3, 6, 4,
-  7, 3, 1, 3, 5, 2, 4, 6, 7, 8, 3, 2, 4, 9, 1, 3, 2, 4, 9, 6,
-  8, 2, 4, 9, 3, 4, 2, 1, 3, 6, 5, 4, 6, 8, 3, 2, 4, 3, 6, 9,
-  5, 4, 3, 7, 4, 9, 8, 7, 3, 2, 7, 4, 3, 9, 2, 4, 8, 2, 4, 6,
+  6657, 5699, 3371, 8909, 7719, 6229, 5449, 8561, 2987, 5501, 3127, 9319, 4365, 9811, 9927, 2423,
+  3439, 1865, 5925, 4409, 5509, 1517, 9695, 9255, 5325, 3691, 5519, 6949, 5607, 9539, 4133, 7795,
+  5465, 2659, 6381, 6875, 4019, 9195, 5645, 2887, 1213, 1815, 8671, 3015, 3147, 2991, 7977, 7045,
+  1619, 7909, 4451, 6573, 4545, 8251, 5983, 2849, 7249, 7449, 9477, 5963, 2711, 9019, 7375, 2201,
+  5631, 4893, 7653, 3719, 8819, 5839, 1853, 9843, 9119, 7023, 5681, 2345, 9873, 6349, 9315, 3795,
+  9737, 4633, 4173, 7549, 7171, 6147, 4723, 5039, 2723, 7815, 6201, 5999, 5339, 4431, 2911, 4435,
+  3611, 4423, 9517, 3243,
 ];
 
 export type ShipGraphType =
@@ -20,13 +22,19 @@ export type ShipGraphType =
   | 'character_up'
   | 'supply_character';
 
+
+
 /** Compute the cipher key for a ship image file. */
 export function calcShipGraphKey(masterId: number, filename: string): string {
   let s = 0;
-  for (let i = 0; i < filename.length; i++) {
-    s += filename.charCodeAt(i);
+  if (filename != null && filename !== '') {
+    for (let i = 0; i < filename.length; i++) {
+      s += filename.charCodeAt(i);
+    }
   }
-  const key = (((17 * (masterId + 7) * RESOURCE[(s + masterId * 5) % 100]) % 8973) + 1000);
+
+  const len = filename != null && filename.length > 0 ? filename.length : 1;
+  const key = (((17 * (masterId + 7) * RESOURCE[(s + masterId * len) % 100]) % 8973) + 1000);
   return key.toString();
 }
 
